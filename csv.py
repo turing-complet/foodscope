4a81476746c364d6fe2e8ab66caf0a8cab6c78bf
Jon Hagg <jhagg314@gmail.com>
2021-04-26 00:34:29 -0700

    filter cols and source_type

--- csv.py
+++ csv.py
@@ -2,14 +2,19 @@ import pandas as pd
 
 
 def _get_compounds(name: str):
-    df = pd.read_csv("foodb_2020_04_07_csv/Compound.csv")
+    df = pd.read_csv("foodb_2020_04_07_csv/Compound.csv", usecols=["id", "name"])
     return df.loc[df["name"].str.contains(name, case=False), :]
 
 
 def get_foods(compound: str):
     compounds = _get_compounds(compound)
     ids = compounds.id
-    df = pd.read_csv("foodb_2020_04_07_csv/Content.csv")
-    foods = pd.read_csv("foodb_2020_04_07_csv/Food.csv")
-    food_id = df.loc[df["source_id"].isin(ids), "food_id"]
+    df = pd.read_csv(
+        "foodb_2020_04_07_csv/Content.csv",
+        usecols=["id", "food_id", "source_id", "source_type"],
+    )
+    foods = pd.read_csv("foodb_2020_04_07_csv/Food.csv", usecols=["id", "name"])
+    food_id = df.loc[
+        df["source_id"].isin(ids) & (df["source_type"] == "Compound"), "food_id"
+    ]
     return foods.loc[foods["id"].isin(food_id), :]
