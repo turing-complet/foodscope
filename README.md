a03b5e27e22201774712557376f33462f501eebd
Jon Hagg <jhagg314@gmail.com>
2021-05-01 15:01:42 -0700

    rename cols and use pandas merge

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ https://foodb.ca/compounds/FDB003717#references
 ## TODO
 - Bin foods by nutrient (quartiles)
 - get health effects of a given meal
+- preprocess data to minimize file size, or import to postgres?
--- csv.py
+++ csv.py
@@ -3,18 +3,30 @@ import pandas as pd
 
 def _get_compounds(name: str):
     df = pd.read_csv("foodb_2020_04_07_csv/Compound.csv", usecols=["id", "name"])
-    return df.loc[df["name"].str.contains(name, case=False), :]
+    df = df.loc[df["name"].str.contains(name, case=False), :]
+    return df.rename(columns={"id": "compound_id"})
 
 
 def get_foods(compound: str):
     compounds = _get_compounds(compound)
-    ids = compounds.id
+    ids = compounds.compound_id
+    cols = [
+        "id",
+        "food_id",
+        "source_id",
+        "source_type",
+        "orig_content",
+        "orig_min",
+        "orig_max",
+    ]
     df = pd.read_csv(
         "foodb_2020_04_07_csv/Content.csv",
-        usecols=["id", "food_id", "source_id", "source_type"],
+        usecols=cols,
     )
+    mask = df["source_id"].isin(ids) & (df["source_type"] == "Compound")
+    df = df.loc[mask, :]
+
     foods = pd.read_csv("foodb_2020_04_07_csv/Food.csv", usecols=["id", "name"])
-    food_id = df.loc[
-        df["source_id"].isin(ids) & (df["source_type"] == "Compound"), "food_id"
-    ]
-    return foods.loc[foods["id"].isin(food_id), :]
+    foods.rename(columns={"id": "food_id"}, inplace=True)
+    merged = pd.merge(df, foods, on="food_id")
+    return merged
